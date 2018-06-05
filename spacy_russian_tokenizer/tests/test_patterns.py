8a2fcf6293212607e459b297bf943ff8c25426b2
aatimofeev <eisenbart@ya.ru>
2018-06-05 18:38:44 +0300

    updated test

--- spacy_russian_tokenizer/tests/test_patterns.py
+++ spacy_russian_tokenizer/tests/test_patterns.py
@@ -1,16 +1,11 @@
-import pytest
-from spacy.util import get_lang_class
-from spacy.matcher import Matcher
-
-from spacy_russian_tokenizer.src.hyphen_patterns import FOREIGN_SURNAME_PATTERNS, PRONOUN_PATTERNS, PARTICLE_PATTERNS, \
+from spacy_russian_tokenizer import pipeline
+from spacy_russian_tokenizer.src import FOREIGN_SURNAME_PATTERNS, PRONOUN_PATTERNS, PARTICLE_PATTERNS, \
     ADVERB_PATTERNS, COMPOUND_PREPOSITION_PATTERNS, PROPER_NAMES_WITH_DIGITS_PATTERNS, \
     COMPOUND_WORDS_WITH_DIGIT_PATTERNS, GEOGRAPHIC_NAMES_PATTERNS, ORG_NAMES_PATTERNS, CONJUNCT_WORDS_PATTERNS, \
-    DIRECTION_PATTERNS, MISC_HYPHEN_FORM_PATTERNS, LOANWORDS_PATTERNS, SYNTAGRUS_RARE_CASES, COMPOUND_NOUNS, \
-    COMPOUND_ADJECTIVE_PATTERNS, ADDRESS_PATTERNS
-from ..src.bigram_patterns import PERCENTAGE_PATTERNS, TIME_PATTERNS
-from spacy_russian_tokenizer.src.sentence_terminal_patterns import NO_TERMINAL_PATTERNS
-from spacy_russian_tokenizer.src.sentence_segmentation import detect_sentence_boundaries
-from spacy_russian_tokenizer.pipeline import pipeline
+    DIRECTION_PATTERNS, LOANWORDS_PATTERNS, COMPOUND_NOUNS, \
+    COMPOUND_ADJECTIVE_PATTERNS, ADDRESS_PATTERNS, INTERJECTIONS_PATTERNS, UNIT_PATTERNS
+from spacy_russian_tokenizer.src import NO_TERMINAL_PATTERNS
+from spacy_russian_tokenizer.src.bigram_patterns import PERCENTAGE_PATTERNS, TIME_PATTERNS
 
 
 def test_foreign_surnames():
@@ -38,6 +33,13 @@ def test_particles():
     text = "ничего-де, Ответь-ка, Вот-те, Чёрт-те, довольно-таки, наконец-таки"
     doc = nlp(text)
     assert [i.text for i in doc if not i.is_punct] == text.split(', ')
+
+
+def test_interjection():
+    nlp = pipeline(INTERJECTIONS_PATTERNS)
+    text = "Ха-ха-ха, ничего-то ты не знаешь, Джон Сноу!"
+    doc = nlp(text)
+    assert doc[0].text == 'Ха-ха-ха'
 
 
 def test_adverb_and_compound_prepositions():
@@ -123,10 +125,9 @@ def test_compound_adjectives():
 
 def test_percentage():
     nlp = pipeline(merge_patterns=PERCENTAGE_PATTERNS)
-    text = "Социал-демократия — социальная политика и идейно-политическое течение"
+    text = "100% токенизация"
     doc = nlp(text)
-    assert doc[0].text == 'Социал-демократия'
-    assert doc[5].text == 'идейно-политическое'
+    assert doc[0].text == '100%'
 
 
 def test_time():
@@ -138,8 +139,20 @@ def test_time():
     assert doc[16].text == doc[16].text
 
 
+def test_units():
+    nlp = pipeline(merge_patterns=UNIT_PATTERNS)
+    text = "около 10 км/ч"
+    doc = nlp(text)
+    assert doc[2].text == 'км/ч'
+
+
 def test_sentence_segmentation():
-    text = """Сбербанк планирует выходить из ситуации, постепенно повышая ставки по кредитам, заявил вчера на Давосском экономическом форуме глава Сбербанка Герман Греф. "Ставки все взаимосвязаны. Если ставки растут по пассивной части, то они растут и по активной части. Четыре-пять месяцев назад мы выдавали ипотеку под 8% годовых на восемь лет, а сегодня у нас ставки привлечения — 8%. Конечно, это делать невозможно. Мы, конечно, будем постепенно поднимать ставки по кредитам физическим лицам",— цитирует господина Грефа "РИА Новости"""""
+    text = "Сбербанк планирует выходить из ситуации, постепенно повышая ставки по кредитам, заявил вчера на " \
+           "Давосском экономическом форуме глава Сбербанка Герман Греф. \"Ставки все взаимосвязаны. Если ставки " \
+           "растут по пассивной части, то они растут и по активной части. Четыре-пять месяцев назад мы выдавали " \
+           "ипотеку под 8% годовых на восемь лет, а сегодня у нас ставки привлечения — 8%. Конечно, это делать " \
+           "невозможно. Мы, конечно, будем постепенно поднимать ставки по кредитам физическим лицам\",— цитирует " \
+           "господина Грефа \"РИА Новости\""
     nlp = pipeline(terminal_patterns=NO_TERMINAL_PATTERNS)
     doc = nlp(text)
     assert len(list(doc.sents)) == 6
@@ -152,6 +165,7 @@ def test_sentence_segmentation_merge():
     nlp = pipeline(terminal_patterns=NO_TERMINAL_PATTERNS)
     doc = nlp(text)
     assert len(list(doc.sents)) == 1
-    text = """28 мая 2013 года в отношении главы администрации Смоленска Александра Данилюка возбуждено уголовное дело по ч. 2 ст. 286 УК РФ ("Превышение должностных полномочий")."""
+    text = "28 мая 2013 года в отношении главы администрации Смоленска Александра Данилюка возбуждено уголовное дело " \
+           "по ч. 2 ст. 286 УК РФ (\"Превышение должностных полномочий\")."
     doc = nlp(text)
     assert len(list(doc.sents)) == 1
