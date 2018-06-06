aecda7d36e52b16847b1ece47fdbcccfb8475f72
aatimofeev <eisenbart@ya.ru>
2018-06-06 14:32:59 +0300

    added patterns subpackage to setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email="aatimofeev@hse.ru",
     url="https://github.com/aatimofeev/spacy_russian_tokenizer/",
     keywords="natural language processing russian tokenizer tokeniser",
-    packages=['spacy_russian_tokenizer'],
+    packages=['spacy_russian_tokenizer', 'spacy_russian_tokenizer.patterns'],
     install_requires=['spacy'],
     tests_require=['pytest'],
 )
